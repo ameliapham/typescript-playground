667af03e729d4d0e07cede94b61649bea3396006
ameliapham <ptt.huong.8895@gmail.com>
2024-02-18 01:09:22 +0100

    Exo print id : improve parameter with `params`

--- src/exo-print-id.ts
+++ src/exo-print-id.ts
@@ -1,6 +1,16 @@
-function printID(name: string, id : number | string) {
+function printID(params: {
+    name: string,
+    id: number | string
+}) {
+    const { name, id } = params
     console.log(`${name}'s ID is ${id}`)
 }
 
-const myID = printID("Huong", "103")
-const hisID = printID("Joseph", 88)
+const myID = printID({
+    "name": "Huong",
+    "id": "103"
+})
+const hisID = printID({
+    "name": "Joseph",
+    "id": 88
+})
