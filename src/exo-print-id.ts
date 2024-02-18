ca4a987b9e210149e4bf5a58d530508c0c639f9d
ameliapham <ptt.huong.8895@gmail.com>
2024-02-18 01:40:07 +0100

    Exo print : create function welcomePeople, distinguish string and array of string inside the function

--- src/exo-print-id.ts
+++ src/exo-print-id.ts
@@ -1,9 +1,11 @@
+// number or string ?
+
 function printID(params: {
     name: string,
     id: number | string
 }) {
     const { name, id } = params
-    if (typeof id === "string"){
+    if (typeof id === "string") {
         console.log(`${name}'s ID is ${id.toUpperCase()}`)
     } else {
         console.log(`${name}'s ID is ${id}`)
@@ -18,3 +20,19 @@ const hisID = printID({
     "name": "Joseph",
     "id": 88
 })
+
+// string or array of string ?
+
+function welcomePeople(params: {
+    names: string | string[]
+}) {
+    const { names: x } = params
+    if (Array.isArray(x)) {
+        console.log(`Hello ${x.join(" and ")}`)
+    } else {
+        console.log(`Welcome lone traveler ${x}`)
+    }
+}
+
+const listTraveler = welcomePeople({"names": ["Huong", "Joseph"] })
+const listTraveler2 = welcomePeople({"names" : "Anto"})
