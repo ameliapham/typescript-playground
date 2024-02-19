9be0a80da11beb59007320d2ed15b1dcbe4dd54e
ameliapham <ptt.huong.8895@gmail.com>
2024-02-19 19:00:00 +0100

    Exo date : Counting how many seconds between startDate and endDate

--- src/exo-date.ts
+++ src/exo-date.ts
@@ -1,3 +1,5 @@
+// Check type of Date
+
 function logValue(x : Date | string){
     if (x instanceof Date){
         console.log(x.toUTCString())
@@ -12,3 +14,30 @@ const time= (new Date()).getTime();
 
 const nowAsString = (new Date()).toString();
 const nowAsString2 = Date();
+
+
+// Counting how many seconds between startDate and endDate
+
+const startDate = new Date();
+
+console.log(startDate.getTime());
+
+let counter=0;
+
+for(let i = 0; i < 10000; i++){
+	for(let j = 0; j < 1000; j++){
+		for(let k = 0; k < 1000; k++){
+			counter++;
+		}
+
+	}
+}
+
+const endDate= new Date();
+
+const durationMs = endDate.getTime() - startDate.getTime();
+
+const durationSecond = durationMs / 1000;
+
+
+console.log(`Le program a mis ${durationSecond} secondes a s'executer`);
