7d6fc53cf510ecd1be29eebbbc9ca5a23a78c319
ameliapham <ptt.huong.8895@gmail.com>
2024-02-22 23:33:10 +0100

    Exo arr : Create function createMyArray

---
+++ src/exo-array/createMyArray.ts
@@ -0,0 +1,161 @@
+import { MyArray } from "./MyArray";
+
+export function createMyArray<T>(...elements: T[]): MyArray<T> {
+
+    const internalArray = elements
+
+    const initialLength = internalArray.length
+
+    const initialLastElement = internalArray[initialLength - 1]
+
+
+    const myArray: MyArray<T> = {
+        "toString": () => internalArray.join(" - "),
+        "myPush": (elem) => {
+            internalArray.push(elem);
+            myArray.myLength += 1;
+            myArray.lastElement = elem
+        },
+        "pop": () => {
+            if (myArray.myLength <= 0) {
+                // do nothing
+                return;
+            }
+            internalArray.pop();
+            myArray.myLength -= 1;
+            myArray.lastElement = internalArray[myArray.myLength - 1]
+        },
+        "removeElementAtIndex": indexOfElementToRemove => {
+            internalArray.splice(indexOfElementToRemove, 1);
+            myArray.myLength = myArray.myLength - 1;
+        },
+        "myLength": initialLength,
+        "getElement": (index) => {
+            if (index > myArray.myLength) {
+                throw new Error(`Warning you asked for the ${index} element but there is only ${myArray.myLength} element`)
+            } else {
+                return internalArray[index]
+            }
+        },
+        "lastElement": initialLastElement,
+
+        "pushArray": (...elements) => {
+
+            for (let i = 0; i < elements.length; i++) {
+                myArray.myPush(elements[i]);
+                // internalArray.push(elements[i])
+            }
+
+            //myArray.myLength+= elements.length;
+            //myArray.lastElement = internalArray[myArray.myLength - 1]
+
+        },
+        "getSum": () => {
+
+            // const internalArrayNumber: number[] = [];
+            /*
+            for (let i = 0; i < myArray.myLength; i++){
+                const num = internalArray[i];
+                if (typeof num !== "number"){
+                    throw new Error(`This function required an array of number`);
+                }
+                internalArrayNumber.push(num);
+            }*/
+
+            /* internalArray.forEach(element => {
+                if (typeof element !== "number") {
+                    throw new Error(`This function required an array of number`);
+                }
+                internalArrayNumber.push(element);
+            })*/
+
+            /* const internalArrayNumber = internalArray.map(element => {
+                if( typeof element !== "number"){
+                    throw new Error(`This function required an array of number`)
+                }
+                return element;
+            })            
+            */
+
+            if (internalArray.length === 0) {
+                throw new Error("There is no smallest element, it's an empty array");
+            }
+
+            if (!internalArray.every(element => typeof element === "number")) {
+                throw new Error(`This function required an array of number`)
+            }
+
+            const internalArrayNumber: number[] = internalArray as any;
+
+            const sum = internalArrayNumber.reduce((sum, cur) => {
+                return sum + cur;
+            }, 0);
+
+            return sum;
+        },
+        "findSmallerElement": () => {
+
+            /*
+            if (internalArray.length === 0) {
+                throw new Error("There is no smallest element, it's an empty array");
+            }
+
+            if (!internalArray.every(element => typeof element === "number")) {
+                throw new Error(`This function required an array of number`)
+            }
+            const internalArrayNumber: number[] = internalArray as any;
+            */
+
+
+            const internalArrayNumber: number[] = [];
+
+            internalArray.forEach(element => {
+                if (typeof element !== "number") {
+                    throw new Error(`This function required an array of number`)
+                }
+
+                internalArrayNumber.push(element);
+
+            });
+
+            let index = 0;
+
+            for (let i = index + 1; i < myArray.myLength; i++) {
+                if (internalArrayNumber[i] < internalArrayNumber[index]) {
+                    index = i
+                }
+            }
+
+            const smallestNumber = internalArrayNumber[index]
+            return { "smallestNumber": smallestNumber, "index": index };
+
+        },
+        "sort": () => {
+
+            internalArray.forEach(element => {
+                if (typeof element !== "number") {
+                    throw new Error(`This function required an array of number`)
+                }
+            })
+
+            for (let i = 0; i < myArray.myLength; i++) {
+                let min = i
+                for (let k = i + 1; k < myArray.myLength; k++) {
+                    if (internalArray[k] < internalArray[min]) {
+                        min = k
+                    }
+                }
+
+                if (min !== i) {
+                    let res = internalArray[i]
+                    internalArray[i] = internalArray[min]
+                    internalArray[min] = res
+                }
+            }
+        }
+    }
+
+
+    return myArray
+
+}
