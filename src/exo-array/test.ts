f9c2a61b852286c8263b8a06e67cda4ddbbbf223
ameliapham <ptt.huong.8895@gmail.com>
2024-02-22 23:34:19 +0100

    Exo arr : create test.ts

---
+++ src/exo-array/test.ts
@@ -0,0 +1,73 @@
+import { createMyArray } from "./createMyArray";
+import { assert } from "tsafe"
+
+
+{
+
+    const myArray= createMyArray(3,2,4,1)
+
+    const got = myArray.findSmallerElement();
+
+    const expected = {
+        "smallerElementNumber": 1,
+        "index": 3
+    };
+
+    assert(got.smallestNumber === expected.smallerElementNumber, "Fail ❌");
+    assert(got.index === expected.index, "Fail ❌");
+
+    console.log("Pass ✅")
+
+}
+
+{
+
+    const myArray = createMyArray(3, 1, 4, 2)
+
+    const got = myArray.findSmallerElement();
+
+    const expected = {
+        "smallerElementNumber": 1,
+        "index": 1
+    };
+
+    assert(got.smallestNumber === expected.smallerElementNumber, "Fail ❌");
+    assert(got.index === expected.index, "Fail ❌");
+
+    console.log("Pass ✅")
+
+}
+
+{
+
+    const myArray = createMyArray(100_000)
+
+    const got = myArray.findSmallerElement();
+
+    const expected = {
+        "smallerElementNumber": 100_000,
+        "index": 0
+    };
+
+    assert(got.smallestNumber === expected.smallerElementNumber, "Fail ❌");
+    assert(got.index === expected.index, "Fail ❌");
+
+    console.log("Pass ✅")
+
+}
+
+{
+
+    const myArray= createMyArray(3,2,4,1)
+
+    myArray.sort();
+
+    const got = myArray.toString();
+
+    const expected = createMyArray(1,2,3,4).toString();
+
+    assert(got === expected, `Fail ❌ got: ${got}, expected: ${expected}`);
+
+    console.log("Pass ✅")
+
+}
