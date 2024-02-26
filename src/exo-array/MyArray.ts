bef280c5c2661eb104a0598ad4a09a344b527712
ameliapham <ptt.huong.8895@gmail.com>
2024-02-26 04:23:08 +0100

    Exo array : add function setElement in type MyArray

--- src/exo-array/MyArray.ts
+++ src/exo-array/MyArray.ts
@@ -3,15 +3,16 @@ export type MyArray<T> = {
     myToString: () => string;
     myPush: (elem: T) => void;
     pop: () => void;
-    removeElementAtIndex: (indexOfElementToRemove: number)=> void;
+    removeElementAtIndex: (indexOfElementToRemove: number) => void;
     myLength: number;
     getElement: (index: number) => T;
+    setElement: (element: T, index: number) => void;
     lastElement: T | undefined;
     pushArray: (...elems: T[]) => void;
     getSum: () => number;
     findSmallerElement: () => { smallestNumber: number, index: number };
     sort: () => void;
-    forEach: (callback: (element: T, index: number)=>void)=>void;
+    forEach: (callback: (element: T, index: number) => void) => void;
     map: (fn: (element: T) => T) => MyArray<T>;
     filter: (predicate: (element: T, index: number) => boolean) => MyArray<T>
 }
