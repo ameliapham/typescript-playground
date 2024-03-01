4291e03261aba91da191b6e551a7149423a60c43
ameliapham <ptt.huong.8895@gmail.com>
2024-03-01 01:59:09 +0100

    Add function push in createMyArray.ts

--- src/exo-array-2/createMyArray.ts
+++ src/exo-array-2/createMyArray.ts
@@ -6,7 +6,8 @@ function createMyArray<T>(...elements: T[]): MyArray<T>{
 
     const myArray: MyArray<T> = {
         "toString": () => internalArray.join(" - "),
-
+        "push": (element) => internalArray.push(element),
+        
     }
 
     return myArray
