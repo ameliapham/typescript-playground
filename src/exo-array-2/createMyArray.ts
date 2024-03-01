cad8621a17a648fc9c6b76cf44d21d107ca38118
ameliapham <ptt.huong.8895@gmail.com>
2024-03-01 01:56:38 +0100

    Add function toString in createMyArray.ts

--- src/exo-array-2/createMyArray.ts
+++ src/exo-array-2/createMyArray.ts
@@ -2,8 +2,10 @@ import {MyArray} from "./MyArray"
 
 function createMyArray<T>(...elements: T[]): MyArray<T>{
 
+    const internalArray = elements
+
     const myArray: MyArray<T> = {
-        
+        "toString": () => internalArray.join(" - "),
 
     }
 
