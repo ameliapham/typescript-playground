9e8970ad79194febeb6bc0229774d179651685a3
ameliapham <ptt.huong.8895@gmail.com>
2024-03-05 01:17:17 +0100

    Create type of function filter in MyArray

--- src/exo-array-2/MyArray.ts
+++ src/exo-array-2/MyArray.ts
@@ -12,5 +12,6 @@ export type MyArray<T> = {
     findSmallestElement: () => { smallestNumber: number, index: number };
     sort: () => void;
     forEach: (callback: (element: T, index: number) => void) => void;
-    map: (callback: (element: T, index: number) => T) => MyArray<T>
+    map: (callback: (element: T, index: number) => T) => MyArray<T>;
+    filter: (predicate: (element: T, index: number) => boolean) => MyArray<T>;
 }
